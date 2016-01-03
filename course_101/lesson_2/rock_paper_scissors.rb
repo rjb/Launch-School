06d634ed2ccf2c51111a4db523880255089cfb76
Raffy Banks <raffybanks@gmail.com>
2016-01-03 12:04:21 -0800

    Added ability to challenge Computer to another game

--- course_101/lesson_2/rock_paper_scissors.rb
+++ course_101/lesson_2/rock_paper_scissors.rb
@@ -52,40 +52,45 @@ def display_final_results(scores)
   end
 end
 
-display_welcome_message
-
 loop do
-  choice = ''
+  display_welcome_message
+
   loop do
-    display_choices
+    choice = ''
+    loop do
+      display_choices
 
-    choice = gets.chomp
-    break if VALID_CHOICES.keys.include?(choice.to_sym)
+      choice = gets.chomp
+      break if VALID_CHOICES.keys.include?(choice.to_sym)
+    end
+
+    computer_choice = VALID_CHOICES.keys.sample.to_s
+
+    prompt("You chose: #{VALID_CHOICES[choice.to_sym]}; Computer chose: #{VALID_CHOICES[computer_choice.to_sym]}")
+
+    if win?(choice, computer_choice)
+      prompt("You won the round!")
+      scores[:human] += 1
+    elsif win?(computer_choice, choice)
+      prompt("Computer won the round")
+      scores[:computer] += 1
+    else
+      prompt("Round is a draw")
+    end
+
+    display_scoreboard(scores)
+
+    if game_over?(scores)
+      display_final_results(scores)
+      break
+    end
+
+    prompt("Press the <enter> to play another round, or forfeit (f)")
+    break if gets.chomp.downcase.start_with?('f')
   end
 
-  computer_choice = VALID_CHOICES.keys.sample.to_s
-
-  prompt("You chose: #{VALID_CHOICES[choice.to_sym]}; Computer chose: #{VALID_CHOICES[computer_choice.to_sym]}")
-
-  if win?(choice, computer_choice)
-    prompt("You won the round!")
-    scores[:human] += 1
-  elsif win?(computer_choice, choice)
-    prompt("Computer won the round")
-    scores[:computer] += 1
-  else
-    prompt("Round is a draw")
-  end
-
-  display_scoreboard(scores)
-
-  if game_over?(scores)
-    display_final_results(scores)
-    break
-  end
-
-  prompt("Press the <enter> to play another round, or forfeit (f)")
-  break if gets.chomp.downcase.start_with?('f')
+  prompt("Up for another game? (y or n)")
+  break unless gets.chomp.downcase.start_with?('y')
 end
 
-# prompt("Thank you for playing. Good bye!")
+prompt("Thank you for playing. Good bye!")
