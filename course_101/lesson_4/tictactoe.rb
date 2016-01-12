fd276c78cbe814329970734c69a48e92373de861
Raffy Banks <raffybanks@gmail.com>
2016-01-11 22:08:40 -0800

    WINNING_LINES defined as one double array

--- course_101/lesson_4/tictactoe.rb
+++ course_101/lesson_4/tictactoe.rb
@@ -3,9 +3,9 @@ INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
 WINNING_SCORE = 5
-WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
-                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
-                [[1, 5, 9], [3, 5, 7]]
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
+                 [1, 4, 7], [2, 5, 8], [3, 6, 9],
+                 [1, 5, 9], [3, 5, 7]]
 
 def prompt(message)
   puts "=> #{message}"
