1f7aecfab1e101893e0c396c4f4d97e7ed20d6f3
Raffy Banks <raffybanks@gmail.com>
2016-01-17 22:47:00 -0800

    Scoring

--- course_102/lesson_2/rock_paper_scissors.rb
+++ course_102/lesson_2/rock_paper_scissors.rb
@@ -1,8 +1,9 @@
 class Player
-  attr_accessor :move, :name
+  attr_accessor :name, :move, :score
 
   def initialize
     set_name
+    self.score = Score.new
   end
 end
 
@@ -32,7 +33,7 @@ end
 
 class Computer < Player
   def set_name
-    self.name = %w(hal c3po 5 r2d2).sample
+    self.name = %w(HAL C3PO Number5 GERTY RobotB-9 Rosie).sample
   end
 
   def choose
@@ -76,16 +77,45 @@ class Move
   end
 end
 
+class Score
+  INITIAL_SCORE = 0
+  WINNING_SCORE = 10
+
+  def initialize
+    @value = INITIAL_SCORE
+  end
+
+  def reset
+    @value = INITIAL_SCORE
+  end
+
+  def add_point
+    @value += 1
+  end
+
+  def ==(value)
+    @value == value
+  end
+
+  def to_s
+    "#{@value}"
+  end
+end
+
 class RPSGame
   attr_accessor :human, :computer
 
   def initialize
+    display_welcome_message
     @human = Human.new
     @computer = Computer.new
   end
 
   def display_welcome_message
+    system 'clear'
     puts "Welcome to Rock, Paper, Scissors!"
+    puts "First to #{Score::WINNING_SCORE} wins."
+    puts "---------------------------------"
   end
 
   def display_goodbye_message
@@ -93,8 +123,9 @@ class RPSGame
   end
 
   def display_moves
-    puts "#{human.name} chose: #{human.move}"
-    puts "#{computer.name} chose: #{computer.move}"
+    moves = "#{human.name} chose: #{human.move}; "
+    moves << "#{computer.name} chose: #{computer.move}"
+    puts moves
   end
 
   def display_winner
@@ -103,14 +134,47 @@ class RPSGame
     elsif human.move < computer.move
       puts "#{computer.name} wins."
     else
-      puts "Draw."
+      puts "It's a draw."
+    end
+  end
+
+  def display_scores
+    puts "#{human.name}: #{human.score}"
+    puts "#{computer.name}: #{computer.score}"
+    puts "---------------------------------"
+  end
+
+  def display_game_board
+    system 'clear'
+    display_welcome_message
+    display_scores
+  end
+
+  def tally_points
+    if human.move > computer.move
+      human.score.add_point
+    elsif human.move < computer.move
+      computer.score.add_point
+    end
+  end
+
+  def game_over?
+    "#{human.score}".to_i == Score::WINNING_SCORE ||
+      "#{computer.score}".to_i == Score::WINNING_SCORE
+  end
+
+  def display_game_winner
+    if human.score == Score::WINNING_SCORE
+      puts "#{human.name} won the game!"
+    elsif computer.score == Score::WINNING_SCORE
+      puts "#{computer.name} won the game."
     end
   end
 
   def play_again?
     answer = nil
     loop do
-      puts "Play again? (y/n)"
+      puts "Up for another game? (y/n)"
       answer = gets.chomp.downcase
       break if %w(y n).include?(answer)
       puts "Please answer y or n."
@@ -118,17 +182,43 @@ class RPSGame
     answer == 'y' ? true : false
   end
 
-  def play
-    display_welcome_message
-
+  def forfeit?
+    answer = nil
     loop do
-      @human.choose
-      @computer.choose
-      display_moves
-      display_winner
-      display_goodbye_message
+      puts "Press <enter> to continue or Forfeit (f)"
+      answer = gets.downcase
+      break if ["\n", "f\n"].include?(answer)
+      puts "Invalid. Please press <enter> or f"
+    end
+    answer == "f\n" ? true : false
+  end
+
+  def play
+    loop do
+      human.score.reset
+      computer.score.reset
+
+      loop do
+        display_game_board
+        @human.choose
+        @computer.choose
+        tally_points
+        display_game_board
+        display_moves
+        display_winner
+
+        if game_over?
+          display_game_winner
+          break
+        end
+
+        break if forfeit?
+      end
+
       break unless play_again?
     end
+    
+    display_goodbye_message
   end
 end
 
