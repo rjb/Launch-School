a0d67f0caf42c4f5b79a098b4210c314c8a60e86
Raffy Banks <raffybanks@gmail.com>
2016-05-31 21:45:33 -0700

    Added class variable to track total number of dog objects created

--- course_102_redux/lesson_1/good_dog.rb
+++ course_102_redux/lesson_1/good_dog.rb
@@ -1,10 +1,12 @@
 class GoodDog
+  @@number_of_dogs = 0
   attr_accessor :name, :height, :weight
 
   def initialize(n, h, w)
     @name = n
     @height = h
     @weight = w
+    @@number_of_dogs += 1
   end
 
   def change_info(n, h, w)
@@ -24,10 +26,24 @@ class GoodDog
   def self.what_am_i
     "I'm a GoodDog class!"
   end
+
+  def self.total_number_of_dogs
+    @@number_of_dogs
+  end
 end
+
+puts GoodDog.total_number_of_dogs
 
 sparky = GoodDog.new('Sparky', '12 inches', '5 lbs.')
 puts sparky.info
 
 sparky.change_info('Spot', '24 inches', '10 lbs.')
 puts sparky.info
+
+jones = GoodDog.new('Sparky', '12 inches', '5 lbs.')
+puts jones.info
+
+sparky.change_info('Spot', '24 inches', '10 lbs.')
+puts jones.info
+
+puts GoodDog.total_number_of_dogs
