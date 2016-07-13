21fd9a2136e627f1bd59ba88eef3268680c11f2b
Raffy Banks <raffybanks@gmail.com>
2016-07-12 18:42:32 -0700

    Created Vehicle superclass

---
+++ course_102_redux/lesson_1/exercises.rb
@@ -0,0 +1,62 @@
+class Vehicle
+  def speed_up
+    @speed += 5
+  end
+
+  def break
+    @speed -= 5 unless @speed == 0
+  end
+
+  def spray_paint(color)
+    self.color = color
+  end
+
+  def current_speed
+    puts "You are now going #{@speed} MPH"
+  end
+
+  def shut_off
+    puts "Engine off"
+  end
+
+  def to_s
+    "Year: #{self.year}; Mode: #{self.model}; Color: #{self.color}"
+  end
+end
+
+class MyCar < Vehicle
+  attr_accessor :color
+  attr_reader :year, :model
+
+  CABIN_TYPE = "Passenger"
+
+  def initialize(year, color, model)
+    @year = year
+    @color = color
+    @model = model
+    @speed = 0
+  end
+
+  def self.miles_per_gallon(distance, gallons)
+    distance / gallons
+  end
+end
+
+class MyTruck < Vehicle
+  CABIN_TYPE = "Truck"
+end
+
+audi = MyCar.new(2005, 'gray', 'A4')
+
+5.times { audi.speed_up }
+audi.current_speed
+10.times { audi.break }
+audi.current_speed
+
+audi.spray_paint('Black')
+puts audi.color
+
+mpg = MyCar.miles_per_gallon(30, 2)
+puts "This car gets #{mpg} mpg"
+
+puts audi
