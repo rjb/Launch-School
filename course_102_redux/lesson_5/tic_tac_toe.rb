19a7e1502113ac8d8d527d57df541376ad380d85
Raffy Banks <raffybanks@gmail.com>
2016-07-31 08:19:28 -0700

    Split display_board into two methods to handle clearing the screen

--- course_102_redux/lesson_5/tic_tac_toe.rb
+++ course_102_redux/lesson_5/tic_tac_toe.rb
@@ -102,8 +102,7 @@ class TTTGame
     puts "Thanks for playing!"
   end
 
-  def display_board(options = {clear_screen: true})
-    clear if options[:clear_screen]
+  def display_board
     puts "You're #{human.marker}. Computer is #{computer.marker}."
     puts ""
     puts "     |     |"
@@ -118,6 +117,11 @@ class TTTGame
     puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}"
     puts "     |     |"
     puts ""
+  end
+
+  def clear_screen_and_display_board
+    clear
+    display_board
   end
 
   def display_result
@@ -164,7 +168,7 @@ class TTTGame
     display_welcome_message
 
     loop do
-      display_board(clear_screen: false)
+      display_board
 
       loop do
         human_moves
@@ -173,8 +177,9 @@ class TTTGame
         computer_moves
         break if board.someone_won? || board.full?
 
-        display_board
+        clear_screen_and_display_board
       end
+      clear
       display_result
       break unless play_again?
       board.reset
