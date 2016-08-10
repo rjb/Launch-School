9daf214544e1058a2acabe309fb5d68d9a95d4c2
Raffy Banks <raffybanks@gmail.com>
2016-08-09 22:24:11 -0700

    Fixed typo

--- course_102_redux/lesson_5/tic_tac_toe.rb
+++ course_102_redux/lesson_5/tic_tac_toe.rb
@@ -96,7 +96,7 @@ class SmallBoard < Board
 end
 
 # 5x5 board
-class Medium < Board
+class MediumBoard < Board
   WINNING_COUNT = 5
   WINNING_LINES = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15],
                    [16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [1, 6, 11, 16, 21],
