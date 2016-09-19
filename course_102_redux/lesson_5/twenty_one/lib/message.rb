9ceb74e2c26f81bb603e5423f10bf141b0cfc9c1
Raffy Banks <raffybanks@gmail.com>
2016-09-18 22:49:18 -0700

    Dropped unused messages

--- course_102_redux/lesson_5/twenty_one/lib/message.rb
+++ course_102_redux/lesson_5/twenty_one/lib/message.rb
@@ -1,13 +1,6 @@
 # Contains all messages
 class Message
-  include Rules
   extend Currency
-
-  DIVIDER = '-----------------------------'
-
-  def self.welcome
-    "Welcome to Twenty-One!\nTable minimum: #{format_as_currency(MIN_BET)}"
-  end
 
   def self.twenty_one
     'Twenty-One!'
@@ -27,49 +20,5 @@ class Message
 
   def self.draw
     'Draw.'
-  end
-
-  def self.place_bet(player)
-    "#{player.name}: Place your bet."
-  end
-
-  def self.name_request
-    'What is your name?'
-  end
-
-  def self.name_invalid
-    'Invalid name.'
-  end
-
-  def self.player_count_request
-    "How many players? (1-#{SEATS})"
-  end
-
-  def self.valid_player_count
-    "Enter a valid number between 1 and #{SEATS}."
-  end
-
-  def self.bet_too_low
-    'Bet is too low.' 
-  end
-
-  def self.wallet_too_low
-    "You're wallet is a little light."
-  end
-
-  def self.move(player)
-    "#{player.name}: Hit (h) or stand (s)?"
-  end
-
-  def self.play_again(player)
-    "#{player.name}: Play another hand (enter) or cash out ($)?"
-  end
-
-  def self.out_of_cash(player)
-    "#{player.name}: You're out of cash. Goodbye."
-  end
-
-  def self.table_closed
-    'Table closed.'
   end
 end
