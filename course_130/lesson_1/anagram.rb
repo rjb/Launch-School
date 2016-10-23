ccd5c01f64683892f9e877ba29d81143cd449edb
Raffy Banks <raffybanks@gmail.com>
2016-10-23 10:47:52 -0700

    Anagram

---
+++ course_130/lesson_1/anagram.rb
@@ -0,0 +1,79 @@
+class Anagram
+  attr_reader :string
+
+  def initialize(string)
+    @string = string
+  end
+
+  def match(strs)
+    strs.select { |str| equal?(str) }
+  end
+
+  private
+
+  def equal?(str)
+    string.downcase != str.downcase &&
+      string.downcase.chars.sort == str.downcase.chars.sort
+  end
+end
+
+require 'minitest/autorun'
+
+class AnagramTest < Minitest::Test
+  def test_no_matches
+    detector = Anagram.new('diaper')
+    assert_equal [], detector.match(%w(hello world zombies pants))
+  end
+
+  def test_detect_simple_anagram
+    detector = Anagram.new('ant')
+    anagrams = detector.match(%w(tan stand at))
+    assert_equal ['tan'], anagrams
+  end
+
+  def test_detect_multiple_anagrams
+    detector = Anagram.new('master')
+    anagrams = detector.match(%w(stream pigeon maters))
+    assert_equal %w(maters stream), anagrams.sort
+  end
+
+  def test_does_not_confuse_different_duplicates
+    detector = Anagram.new('galea')
+    assert_equal [], detector.match(['eagle'])
+  end
+
+  def test_identical_word_is_not_anagram
+    detector = Anagram.new('corn')
+    anagrams = detector.match %w(corn dark Corn rank CORN cron park)
+    assert_equal ['cron'], anagrams
+  end
+
+  def test_eliminate_anagrams_with_same_checksum
+    detector = Anagram.new('mass')
+    assert_equal [], detector.match(['last'])
+  end
+
+  def test_eliminate_anagram_subsets
+    detector = Anagram.new('good')
+    assert_equal [], detector.match(%w(dog goody))
+  end
+
+  def test_detect_anagram
+    detector = Anagram.new('listen')
+    anagrams = detector.match %w(enlists google inlets banana)
+    assert_equal ['inlets'], anagrams
+  end
+
+  def test_multiple_anagrams
+    detector = Anagram.new('allergy')
+    anagrams =
+      detector.match %w( gallery ballerina regally clergy largely leading)
+    assert_equal %w(gallery largely regally), anagrams.sort
+  end
+
+  def test_anagrams_are_case_insensitive
+    detector = Anagram.new('Orchestra')
+    anagrams = detector.match %w(cashregister Carthorse radishes)
+    assert_equal ['Carthorse'], anagrams
+  end
+end
