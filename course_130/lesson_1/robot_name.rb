91119db6ac6b8dd817a92da51cbb85b49a44df29
Raffy Banks <raffybanks@gmail.com>
2016-11-06 00:54:14 -0700

    Robot name

---
+++ course_130/lesson_1/robot_name.rb
@@ -0,0 +1,61 @@
+class Robot
+  attr_reader :name
+
+  @names = []
+
+  def self.names
+    @names
+  end
+
+  def initialize
+    reset
+  end
+
+  def reset
+    @name = (1..2).map { ('A'..'Z').to_a[rand(1..9)] }.join +
+            (1..3).map { rand(1..9) }.join
+    self.class.names.include?(name) ? reset : self.class.names << @name
+  end
+end
+
+require 'minitest/autorun'
+
+class RobotTest < Minitest::Test
+  DIFFERENT_ROBOT_NAME_SEED = 1234
+  SAME_INITIAL_ROBOT_NAME_SEED = 1000
+
+  NAME_REGEXP = /^[A-Z]{2}\d{3}$/
+
+  def test_has_name
+    assert_match NAME_REGEXP, Robot.new.name
+  end
+
+  def test_name_sticks
+    robot = Robot.new
+    robot.name
+    assert_equal robot.name, robot.name
+  end
+
+  def test_different_robots_have_different_names
+    Kernel.srand DIFFERENT_ROBOT_NAME_SEED
+    refute_equal Robot.new.name, Robot.new.name
+  end
+
+  def test_reset_name
+    Kernel.srand DIFFERENT_ROBOT_NAME_SEED
+    robot = Robot.new
+    name = robot.name
+    robot.reset
+    name2 = robot.name
+    refute_equal name, name2
+    assert_match NAME_REGEXP, name2
+  end
+
+  def test_different_name_when_chosen_name_is_taken
+    Kernel.srand SAME_INITIAL_ROBOT_NAME_SEED
+    name1 = Robot.new.name
+    Kernel.srand SAME_INITIAL_ROBOT_NAME_SEED
+    name2 = Robot.new.name
+    refute_equal name1, name2
+  end
+end
